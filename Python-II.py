756bdbe821b731dfc6b50dab51767ec68039ec7e
Ponnmozhi <iponnmozhi@gmail.com>
2025-02-26 03:58:10 -0500

    Add files via upload

---
+++ Python-II.py
@@ -0,0 +1,38 @@
+#Index value of the letter "t" in "Python":
+val = "Python"
+index = val.index("t")
+print("Index value of t is :",index)
+
+#Extract the 4th value from the string "Welcome".
+val = "Welcome"
+index = val.index("c")
+print("Index value of c is :",index)
+
+# Calculate the length of this word “Python”.
+word = "Python"
+length = len(word)
+print("length of the word python is :",length)
+
+#“I’m doing great‘ -. Write a python code to convert this to upper case
+txt = "I’m doing great"
+print("UpperCase for 'I'm doing great':",txt.upper())
+
+# Fill in the blanks - “_____ is my country” and “____ is learning ____”.
+a = "Fill in the blanks :"
+country = "India"
+name = "She"
+language = "Python"
+print(a)
+print(f"1. {country} is my country.")
+print(f"2. {name} is learning {language}.")
+
+#Check “learn” exists in the given string - “I’m learning python”.
+txt = "I’m learning python"
+if "learn" in txt:
+    print("True")
+else:
+    print("False")
+
+
+
+
